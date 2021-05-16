034c34043539ec2c7d10987a26ea3d20ac30a281
Eduardo Lucio <eduardomalone@hotmail.com>
2021-05-16 01:01:24 -0300

    Primeiro commit do projeto

---
+++ backend/src/main/java/com/tecnologia/conecteEducacao/ConecteEducacaoApplication.java
@@ -0,0 +1,13 @@
+package com.tecnologia.conecteEducacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConecteEducacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConecteEducacaoApplication.class, args);
+	}
+
+}
